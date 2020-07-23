acf78a703b1f81d658ba30ee8c5bc72b3fb1ce83
lizaetillman <lizaetillman@gmail.com>
2020-07-23 00:04:51 -0400

    Done.

---
+++ lib/mp3_importer.rb
@@ -0,0 +1,18 @@
+class MP3Importer
+    
+    attr_accessor :path
+
+    def initialize(path)
+        @path = path 
+    end
+
+    def files()
+        mp3 = Dir.new("spec/fixtures/mp3s")
+        mp3.children
+    end
+
+    def import
+        files.each { |song| Song.new_by_filename(song) }
+    end
+    
+end
